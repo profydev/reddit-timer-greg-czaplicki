18b30e94df35946c67f13ded18f6d381c3bbec43
gregczaplicki <mfkxq8462>
2021-09-22 20:48:46 -0400

    Add react router
    home and search routes

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Link,
+} from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <h1>Title</h1>
-      <a href="/" className="btn">Search</a>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/search">Search</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </Router>
   );
 }
 
